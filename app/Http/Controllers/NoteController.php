c1d3ff700f70009ead071d8e8318666fffdf609f
Victor Matheus <matheuslima.o183@gmail.com>
2023-01-31 16:22:05 -0300

    store method created

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -8,5 +8,12 @@ class NoteController extends Controller
 {
     public function index(){
         return view('index');
+    public function store(Request $request){
+        $note = new Note();
+        $note->title = $request->title;
+        $note->description = $request->description;
+        $note->owner_id = 1;
+        $note->save();
+        return redirect('/note-creation')->with('msg', 'Note created');
     }
 }
