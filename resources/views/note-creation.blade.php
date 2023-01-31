9f99cb452351a32e2405c96037aea3d51267fe52
Victor Matheus <matheuslima.o183@gmail.com>
2023-01-31 16:21:47 -0300

    Note creation view
    It renders a form that submits the note to the 'store' route

---
+++ resources/views/note-creation.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.main')
+
+@section('title', 'Note Creation')
+
+@section('content')
+
+@yield('msg')
+
+<form action="/note/store" method="post">
+    @csrf
+    <label for="note-title">Title</label>
+    <input type="text" name="title" id="note-title">
+    <br>
+    <label for="note-description">Description</label>
+    <textarea name="description" id="note-description" cols="30" rows="10"></textarea>
+    <button type="submit" class="">Create</button>
+</form>
+
+@endsection
