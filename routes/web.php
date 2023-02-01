ade026171c9b2a4ac83199dc2f7c4ee06bf4bb29
Victor Matheus <matheuslima.o183@gmail.com>
2023-02-01 11:23:53 -0300

    routes authentication

--- routes/web.php
+++ routes/web.php
@@ -15,22 +15,23 @@ use App\Http\Controllers\NoteController;
 */
 
 
-Route::get('/', [NoteController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/', [NoteController::class, 'index']);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/note/create', [NoteController::class, 'createNote']);
+    Route::post('/note/store', [NoteController::class, 'store']);
+    Route::delete('/note/delete/{id}', [NoteController::class, 'delete']);
+    Route::get('/note/{id}', [NoteController::class, 'show']);
+    Route::get('/note/edit/{id}', [NoteController::class, 'editNote']);
+    Route::put('/note/update/{id}', [NoteController::class, 'update']);
 });
 require __DIR__.'/auth.php';
 
-Route::get('/note/create', [NoteController::class, 'createNote']);
-Route::post('/note/store', [NoteController::class, 'store']);
-Route::delete('/note/delete/{id}', [NoteController::class, 'delete']);
-Route::get('/note/{id}', [NoteController::class, 'show']);
-Route::get('/note/edit/{id}', [NoteController::class, 'editNote']);
-Route::put('/note/update/{id}', [NoteController::class, 'update']);
