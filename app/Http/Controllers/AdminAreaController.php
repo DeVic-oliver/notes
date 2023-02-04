0ca4bc49881c9023ff8c0fa57747028316ae3f4d
Victor Matheus <victormatheus183@gmail.com>
2023-02-04 10:22:55 -0300

    admin funcionalities upon user

--- app/Http/Controllers/AdminAreaController.php
+++ app/Http/Controllers/AdminAreaController.php
@@ -6,11 +6,32 @@ use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
-
+use App\Models\User;
+use App\Models\Note;
+use Illuminate\Support\Facades\Redis;
 
 class AdminAreaController extends Controller
 {
     public function show(){
+        return view('admin.dashboard');
+    }
+
+    public function listUsers(){
+        $users = User::where('role', '<>', 'administrator')->get();
+        return view('admin.users-list', ['users' => $users]);
+    }
+
+    public function showUserProfile(Request $request){
+        $user = User::findOrfail($request->id);
+        return view('admin.user-edit', ['user' => $user]);
+    }
+
+    public function editUser(Request $request) : RedirectResponse{
+        $user = User::findOrfail($request->id);
+        $user->role = $request->user_role;
+        return redirect("/admin/users/$user->id")->with('msg', 'User updated!'); 
+    }
+
         if(Auth::user()->role != 'administrator'){
             return redirect('/dashboard')->with('msg', 'This area is restricted to admins!');
         }
