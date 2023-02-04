12efafc0c52941b9e72a492dcf9b11b7595fe854
Victor Matheus <victormatheus183@gmail.com>
2023-02-04 12:47:59 -0300

    middleware for admins
    only users that are admin too can access /admin routes

--- routes/web.php
+++ routes/web.php
@@ -32,16 +32,18 @@ Route::middleware('auth')->group(function () {
     Route::get('/note/edit/{id}', [NoteController::class, 'editNote']);
     Route::put('/note/update/{id}', [NoteController::class, 'update']);
 
-    Route::get('/admin', [AdminAreaController::class, 'show']);
-    Route::get('/admin/users', [AdminAreaController::class, 'listUsers']);
-    Route::get('/admin/users/{id}', [AdminAreaController::class, 'showUserProfile']);
-    Route::put('/admin/users/edit/{id}', [AdminAreaController::class, 'editUser']);
-    Route::delete('/admin/users/delete/{id}', [AdminAreaController::class, 'deleteUser']);
-    //Route::get('/admin/warnings/create', [AdminAreaController::class, 'createWarning']);
-    //Route::post('/admin/warnings/store', [AdminAreaController::class, 'createWarning']);
-    //Route::get('/admin/warnings', [AdminAreaController::class, 'listWarnings']);
-    //Route::get('/warnings/{id}', [AdminAreaController::class, 'showWarning']);
-    //Route::put('/admin/warnings/edit/{id}', [AdminAreaController::class, 'editWarning']);
+    Route::middleware('auth-admin')->group(function () {
+        Route::get('/admin', [AdminAreaController::class, 'show']);
+        Route::get('/admin/users', [AdminAreaController::class, 'listUsers']);
+        Route::get('/admin/users/{id}', [AdminAreaController::class, 'showUserProfile']);
+        Route::put('/admin/users/edit/{id}', [AdminAreaController::class, 'editUser']);
+        Route::delete('/admin/users/delete/{id}', [AdminAreaController::class, 'deleteUser']);
+        //Route::get('/admin/warnings/create', [AdminAreaController::class, 'createWarning']);
+        //Route::post('/admin/warnings/store', [AdminAreaController::class, 'createWarning']);
+        //Route::get('/admin/warnings', [AdminAreaController::class, 'listWarnings']);
+        //Route::get('/warnings/{id}', [AdminAreaController::class, 'showWarning']);
+        //Route::put('/admin/warnings/edit/{id}', [AdminAreaController::class, 'editWarning']);
+    });
 
 
 });
