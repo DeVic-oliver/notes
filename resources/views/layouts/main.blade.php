b9b7025010a3fa3dc4332c1f267d8f3733b02257
Victor Matheus <victormatheus183@gmail.com>
2023-02-06 17:36:30 -0300

    tailwind setup

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,17 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="/assets/css/style.css">
     @yield('head')
 </head>
-<body>
+<body class="bg-amber-50">
     @yield('header')
     
     @if (session('msg'))
         {{session('msg')}}
     @endif
-    
+
     @yield('content')
-    
+
     <footer>
         Developed by Victor Matheus - <a href="#">Github</a>
     </footer>
