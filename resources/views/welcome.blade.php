243ada00bf013dba99bb85d9bbb2a1760d7bd92f
Victor Matheus <matheuslima.o183@gmail.com>
2023-02-09 16:38:59 -0300

    welcome tailwind css classes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,11 @@
 @extends('layouts.main')
 @section('title', 'Web Notes')
-@section('head')
-@endsection
-
-<header class="bg-[#fff] mb-12 px-6 py-8">
+@section('header')
+<header class="bg-amber-100 p-6 mb-[60px]">
     <nav class="container-lg grid grid-cols-2 justify-between">
 
         <div>
-            <img src="/img/post-it-logo.png" alt="Post it icons created by Freepik - Flaticon" srcset="" class="w-14 sm:w-24">
+            <img src="/img/post-it-logo.png" alt="Post it icons created by Freepik - Flaticon" srcset="" class="w-14">
         </div>
 
         <ul class="hidden lg:grid grid-flow-col gap-4 items-center justify-end px-6 text-lg">
@@ -22,7 +20,7 @@
         <div id="open-mobile-menu" class="block self-center justify-self-end lg:hidden">
             <img src="/assets/img/svg/bars-solid.svg" alt="" srcset="" class="w-5">
         </div>
-        <div id="mobile-menu" class="mobile-menu">
+        <div id="mobile-menu" class="mobile-menu z-50">
             <div id="close-mobile-menu" class="relative">
                 <img src="/assets/img/svg/xmark-solid.svg" alt="" srcset="" class="absolute top-7 right-7 w-5">
             </div>
@@ -39,27 +37,27 @@
         </div>
     </nav>
 </header>
+@endsection
 
 @section('content')
-
-    <main class="grid grid-cols-1 lg:grid-cols-2 justify-center px-8">
+    <main class="lg:max-w-[1130px] mx-auto w-full grid grid-cols-1 lg:grid-cols-2 justify-center px-8 md:px-8 lg:px-4">
         <div class="">
-            <iframe class="w-full" src="https://www.youtube.com/embed/FJG5l6dfzp0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        </div>
-        <div class="pt-8 pb-8 lg:pt-0 lg:pb-0 lg:pl-8 lg:pr-8">
-            <h1 class="text-3xl text-center lg:text-start">Welcome to web notes</h1>
-            <h2 class="text-xl text-center lg:text-start">Create some notes to remember anything</h2>
-            <nav class="mt-8">
-                <ul class="flex flex-row w-[90%] mx-auto justify-around">
+            <iframe class="rounded-xl mx-auto w-full h-full sm:w-[360px] sm:h-[215px] lg:w-[460px] lg:h-[215px] xl:w-[560px] xl:h-[315px] " src="https://www.youtube.com/embed/FJG5l6dfzp0" title="YouTube video player" frameborder="0" allow="" allowfullscreen></iframe></div>
+        <div class="pt-8 pb-8 lg:pt-0 lg:pb-0 lg:pl-8 lg:pr-8 lg:justify-self-center self-center">
+            <div class="w-[80%] mx-auto lg:w-full">
+                <h1 class="text-xl text-center font-bold lg:text-start lg:text-4xl">Welcome to web notes</h1>
+                <h2 class="text-lg text-center lg:text-start">Create some notes to remember anything</h2>
+            </div>
+            <nav class="mt-8 h-auto">
+                <ul class="h-auto flex flex-row w-[90%] mx-auto justify-center sm:max-w-xs sm:justify-center lg:justify-start lg:w-full lg:mx-0">
                     <li>
-                        <a href="/register">sign up</a>
+                        <a href="/login" class="default-btn btn hover:bg-blue-300 duration-300">sign in</a>
                     </li>
                     <li>
-                        <a href="/login">sign in</a>
+                        <a href="/register" class="default-btn btn-dark hover:bg-gray-600 duration-300 ml-6">sign up</a>
                     </li>
                 </ul>
             </nav>
         </div>
     </main>
-
 @endsection
