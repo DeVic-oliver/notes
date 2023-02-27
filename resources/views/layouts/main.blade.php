343a356e12d7247cfd831668589f2108a418ae5c
Victor Matheus <matheuslima.o183@gmail.com>
2023-02-27 14:50:11 -0300

    redundant css removed

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="/assets/css/build/style.css">
     <link rel="stylesheet" href="/assets/css/style.css">
     @yield('head')
 </head>
