9baa5f9aa338e16a352841a1efd0c8dda0ed3444
Victor Matheus <matheuslima.o183@gmail.com>
2023-02-28 10:48:04 -0300

    views eng to portuguese

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
 @section('content')
 
     <main class="container-lg mt-8 mb-8 px-4">
-        <h1 class="text-4xl text-center lg:text-start">My notes</h1>
+        <h1 class="text-4xl text-center lg:text-start">Minha anotações</h1>
         @if (is_countable($notes) && count($notes) > 0)
             <div class="grid grid-cols-1 gap-6 mx-auto mt-[40px] sm:grid-cols-2 lg:grid-cols-4">
                 @foreach ($notes['notes'] as $note)
@@ -20,13 +20,13 @@
                 @endforeach    
             </div>
         @else
-            <h2 class="mt-6 text-center text-lg lg:text-start">You don't have any notes!</h2>    
+            <h2 class="mt-6 text-center text-lg lg:text-start">Você não tem anotações!</h2>    
         @endif
     </main>
 
     @if ((is_countable($warnings['warnings']) && count($warnings['warnings']) > 0))
         <aside class="container-lg px-4">
-            <h3 class="text-2xl text-center lg:text-start mb-6">Last admin warnings</h3>
+            <h3 class="text-2xl text-center lg:text-start mb-6">Últimos avisos da adm!</h3>
             <div class="grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-4">
                 @foreach ($warnings['warnings'] as $warning)
                     <x-notes.card   :id="$warning->id" 
--- resources/views/note-creation.blade.php
+++ resources/views/note-creation.blade.php
@@ -7,11 +7,15 @@
 @endsection
 
 @section('content')
-
-<form action="/note/store" method="post">
-    @csrf
-    <x-forms.note-form-base :is-admin="$admin" />
-    <button type="submit" class="">Create</button>
-</form>
+<main class="container-lg px-4 mt-8">
+    <h1 class="text-4xl text-center my-7 font-bold">Nova anotação</h1>
+    <form action="/note/store" method="post" class="max-w-lg w-full mx-auto grid grid-cols-1 gap-6">
+        @csrf
+        <x-forms.note-form-base :is-admin="$admin" />
+        <div class="text-center">
+            <button type="submit" class="default-btn btn btn-dark p-3 w-[80%]">Criar</button>
+        </div>
+    </form>
+</main>
 
 @endsection
