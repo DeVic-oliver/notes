a27a9fc370c8d548d1474676e24e1851079149c4
Victor Matheus <matheuslima.o183@gmail.com>
2023-02-28 09:25:29 -0300

    notes front-end classes css add

--- resources/views/note-edition.blade.php
+++ resources/views/note-edition.blade.php
@@ -9,7 +9,7 @@
     <div class="container-lg px-4 mt-11 grid grid-cols-1 lg:gap-6 lg:grid-cols-3">
         <main class="col-span-2">
             <h1 class="text-4xl text-center my-7 font-bold md:text-start">EDIT NOTE</h1>
-            <form action="/note/update/{{$id}}" method="POST" class="max-w-3xl w-full">
+            <form action="/note/update/{{$id}}" method="POST" class="grid grid-cols-1 gap-4 max-w-3xl w-full">
                 @csrf
                 @method('PUT')
                 <x-forms.note-form-base :is-admin="$admin"/>
--- resources/views/note.blade.php
+++ resources/views/note.blade.php
@@ -7,25 +7,26 @@
 @endsection
 
 @section('content')
-    <main class="">
-        <div class="">
-            <div class="">
-                <h1>{{$note->title}}</h1>
-                <p>{{$note->description}}</p>
-            </div>
-
-            <div class="">
-                <form action="/note/delete/{{$note->id}}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">delete</button>
-                </form>
-                <form action="/note/edit/{{$note->id}}" method="GET">
-                    @csrf
-                    <button type="submit">Edit</button>
-                </form>
-            </div>
-        </div>
-        @include('webnotes-components.notes-list')
-    </main>
+    <div class="container-lg px-4 mt-11 grid grid-cols-1 lg:gap-6 lg:grid-cols-3">
+        <main class="col-span-2 mt-8">
+            <article class="grid grid-cols-1 gap-3 bg-white rounded-xl p-5 pb-8 relative border w-full">
+                <h1 class="text-4xl text-center my-7 font-bold md:text-start">{{$note->title}}</h1>
+                <p class="text-base lg:text-lg" >{{$note->description}}</p>
+                <div class="flex flex-row mt-5">
+                    <form action="/note/delete/{{$note->id}}" method="POST" class="w-[45%] mx-auto">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="default-btn btn-delete btn p-3 w-full">Deletar</button>
+                    </form>
+                    <form action="/note/edit/{{$note->id}}" method="GET" class="w-[45%] mx-auto">
+                        @csrf
+                        <button type="submit" class="default-btn btn p-3 w-full">Editar</button>
+                    </form>
+                </div>
+            </article>
+        </main>
+        <aside class="mt-8">
+            @include('webnotes-components.notes-list')
+        </aside>
+    </div>
 @endsection
