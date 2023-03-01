8919556f91daf5946269fd2b8c1ba992ab38a1cb
Victor Matheus <matheuslima.o183@gmail.com>
2023-03-01 16:27:24 -0300

    blade template for default admin menu items

---
+++ resources/views/admin/menu/default-items.blade.php
@@ -0,0 +1,12 @@
+<li class="">
+    <a href="/dashboard">Dashboard</a>
+</li>            
+<li class="">
+    <a href="/note/create">Adicionar Nota</a>
+</li>
+<li class="">
+    <a href="/profile">Perfil</a>
+</li>
+<li class="">
+    <a href="/admin/users">Usuários</a>
+</li>
